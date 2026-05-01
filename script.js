ab0e9226d3d9152674ce59674f193c25ff0dd589
perfectpizzapoint <perfectpizzapointp1@gmail.com>
2026-05-01 17:18:23 +0530

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 // ⚠️ IMPORTANT: Set your deployed Google Apps Script Web App URL here
-const API_URL = "https://script.google.com/macros/s/AKfycbzfcThwO6xpJ0FRACtR3toOkl9SToRUnP8kESQ17Y19AQiVmWz6LmHz-WBdn5rUHd3w/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzyyi_faies09WyJOrGJaKxUDCiN33VFVgGuaa9ZP6PlLDrHvMkjR8sX7ffaFomVty3/exec";
 
 // ===== STATE =====
 let appConfig = null;
